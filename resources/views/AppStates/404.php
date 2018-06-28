715ff601ca756764553ab417b66f626c09d6c6c2
Farawe iLyas <faraweilyas@gmail.com>
2018-06-28 19:32:01 +0100

    Bug fixes for 404 Error page

--- resources/views/AppStates/404.php
+++ resources/views/AppStates/404.php
@@ -6,6 +6,6 @@
 <body>
     <h3>404 Error: Page not found</h3>
     <p>Sorry, but the page <b>'<?= getSiteURL(); ?>'</b> you are looking for has not been found.</p>
-    <a href="<?php __url("./") ?>">Return to homepage</a>
+    <a href="<?= __url("./") ?>">Return to homepage</a>
 </body>
 </html>
