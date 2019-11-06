790aecf65985ecbd80841768017208308cee56f8
Farawe iLyas <faraweilyas@gmail.com>
2019-11-06 06:02:10 +0100

    Adds class for serial number generator

---
+++ app/Enhancers/SerialNumber.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Enhancers;
+
+/**
+* SerialNumber Class
+*/
+class SerialNumber
+{
+	/**
+	* Generates serial number with ID and length 13
+	* @param string $ID
+	* @return string
+	*/
+	public static function generate(string $ID=NULL) : string
+	{
+		return strtoupper($ID.uniqid());
+	}
+
+	/**
+	* Generate serial number
+	* @return string
+	*/
+	public static function generateSerialNumber () : string
+	{
+		return strtoupper("CHCO".substr(uniqid(), 7, 13)."SN");
+	}
+}
