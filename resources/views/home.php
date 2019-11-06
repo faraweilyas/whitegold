3a1ddb7d068b8d65c56f8746adf133580ca2a560
Farawe iLyas <faraweilyas@gmail.com>
2019-11-06 06:16:54 +0100

    Fix details bug

--- resources/views/home.php
+++ resources/views/home.php
@@ -3,7 +3,7 @@
 <head>
 	<title><?= $title; ?></title>
 	<meta name="description" content="<?= $description; ?>" />
-    <meta name="author" content="<?= App\Enhancers\Detail::author(); ?>" />
+    <meta name="author" content="<?= detail()->author(); ?>" />
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link rel="stylesheet" type="text/css" href="<?= __file(CSS.'fira_code.css'); ?>" />
