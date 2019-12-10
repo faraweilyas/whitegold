fb664e499d71d7ab0b962db8524d43312cb98a71
Farawe iLyas <faraweilyas@gmail.com>
2019-12-10 16:25:07 +0100

    - Adds method to demonstrate pagination - Refactors code

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,23 +12,23 @@ use Blaze\Database\DatabaseObject;
 class User extends DatabaseObject
 {
 	/**
-	* Database Table Name
-	* @var string $tableName
-	*/
+	 * Database Table Name
+	 * @var string $tableName
+	 */
 	protected static $tableName			= 'users';
 	
 	/**
-	* Database Columns
-	* @var array $databaseFields
-	*/
+	 * Database Columns
+	 * @var array $databaseFields
+	 */
 	protected static $databaseFields 	= ['id', 'fullName', 'sex', 'email', 'password', 'lastUpdated', 'lastLogin', 'created'];
 
 	/**
-	* Create new user.
-	* @param \stdClass $userObject
-	* @return bool
-	*/
-	public function newUser (\stdClass $userObject) : bool
+	 * Create new user.
+	 * @param \stdClass $userObject
+	 * @return bool
+	 */
+	public function newUser(\stdClass $userObject) : bool
 	{
 		$this->fullName 		= $userObject->fullName;
 		$this->sex 				= $userObject->sex;
@@ -41,21 +41,21 @@ class User extends DatabaseObject
 	}
 
 	/**
-	* Update user last login date.
-	* @return bool
-	*/
-	public function updateLastLogin () : bool
+	 * Update user last login date.
+	 * @return bool
+	 */
+	public function updateLastLogin() : bool
 	{
 		$this->lastLogin = date("Y-m-d H:i:s");
 		return $this->save() ? TRUE : FALSE;
 	}
 
 	/**
-	* Change user password.
-	* @param string $newPassword
-	* @return bool
-	*/
-	public function changeUserPassword (string $newPassword) : bool
+	 * Change user password.
+	 * @param string $newPassword
+	 * @return bool
+	 */
+	public function changeUserPassword(string $newPassword) : bool
 	{
 		$this->password 	= Encrypt::passwordEncrypt($newPassword);
 		$this->lastUpdated 	= date("Y-m-d H:i:s"); 
@@ -63,12 +63,28 @@ class User extends DatabaseObject
 	}
 
 	/**
-	* Validate user password.
-	* @param string $password
-	* @return bool
-	*/
-	public function validateUser (string $password) : bool
+	 * Validate user password.
+	 * @param string $password
+	 * @return bool
+	 */
+	public function validateUser(string $password) : bool
 	{
 		return (!Encrypt::passwordCheck($password, $this->password)) ? FALSE : TRUE;
+	}
+
+	/**
+	 * Users get method to demonstrate with paginate.
+	 * @param int $page
+	 * @param int $limit
+	 * @param string $order
+	 * @return bool
+	 */
+	public static function getUsers(int $page=1, int $limit=5, string $order="DESC")
+	{
+		$order 		= static::validateOrder($order);
+		$sqlQuery 	= "SELECT * FROM ".static::getTableName();
+		paginate()->setPagination($page, $limit, 1, count(static::findBySql($sqlQuery)));
+		$sqlQuery 	.= " ORDER BY id {$order} ".paginate()->getSQLLimitQuery();
+		return static::findBySql($sqlQuery);
 	}
 }
