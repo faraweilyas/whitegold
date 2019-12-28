905bbd84fa41635102230dad85bd3059d8799d8d
Farawe iLyas <faraweilyas@gmail.com>
2019-12-29 00:03:41 +0100

    Adds detail functionality

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -17,6 +17,7 @@ class Home
 	{
     	$title          = APP_NAME;
 	    $description    = "whiteGold is a PHP light web application framework with sophisticated syntax and this is inspired by the Laravel Framework with the aim to ease the way components are being installed alongside our project which disrupts scaling and development process. whiteGold attempts to tackle this by tearing down components to make them stand alone so it's easy to remove, add and update different components at any point in time.";
+	    detail($title, $description);
 		View::make("home", get_defined_vars());
 	}
 }
--- bootstrap/Initializers.php
+++ bootstrap/Initializers.php
@@ -3,11 +3,13 @@
 use Blaze\Http\Secure;
 use Blaze\Http\Session;
 use App\Enhancers\Route;
+use App\Enhancers\Detail;
 use App\Enhancers\Template;
 use Blaze\Pagination\Paginate;
 use App\Enhancers\BootstrapTemplate;
 
 $secure 				= new Secure;
+$detail 				= new Detail;
 $session 				= new Session(
 	getConstant("SESSION_EXPIRE", TRUE),
 	getConstant("SESSION_PATH", TRUE),
